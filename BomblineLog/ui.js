14a6f34c41679cda722e0a1758f5945cdbc297b0
hipjiOfficial <115195044+hipjiOfficial@users.noreply.github.com>
2025-11-05 14:18:30 -0700

    Update menu items in onOpen function

--- BomblineLog/ui.js
+++ BomblineLog/ui.js
@@ -2,9 +2,10 @@ function onOpen() {
   const ui = SpreadsheetApp.getUi();
   ui.createMenu('Custom Tools')
     .addItem('Colorize Log', 'colorCellsBasedOnReferenceList')
-    .addItem('Sort', 'sortMultipleItemBlocksPreserveFormulas')
+    .addItem('Sort', 'sortStats')
     .addItem('Count Lost Media', 'daysLost')
     .addToUi();
 }
 /*
 This makes a little button at the top that gives you a menu where you can run each function. 
+
