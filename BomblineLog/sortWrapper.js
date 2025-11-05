0d474479802737a5d6363ab9a78ad11088610e3e
hipjiOfficial <115195044+hipjiOfficial@users.noreply.github.com>
2025-11-05 14:15:02 -0700

    Missing lines
    Add sortStats function to execute sorting and probability updates

---
+++ BomblineLog/sortWrapper.js
@@ -0,0 +1,5 @@
+function sortStats() {
+  sortMultipleItemBlocksPreserveFormulas();
+  updateCardAndSkinProbabilities();
+}
+// I wrote the sorting script before the probability script, so I use this to execute them at the same time
